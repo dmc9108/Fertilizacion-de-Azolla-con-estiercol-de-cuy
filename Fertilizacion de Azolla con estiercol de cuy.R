42d674fdf0891220a85eea47374ea0034444798c
dmc9108 <101211245+dmc9108@users.noreply.github.com>
2022-10-12 00:58:38 -0400

    Casi todo, pero ya no salen las letras en el boxplot

--- Fertilizacion de Azolla con estiercol de cuy.R
+++ Fertilizacion de Azolla con estiercol de cuy.R
@@ -43,20 +43,20 @@ axis(1, at = c(8, 16, 24, 32, 40))
 t.test(preexp$`Rendimiento/día MS`)  # Media e IC al 95% para la media 
   
 
-# Gr?fico de l?neas "Estimaci?n de Azolla en Materia seca"  
+# Gráfico de líneas "Estimación de Azolla en Materia seca"  
 
 datos_seca <- data.frame("Rendimiento obtenido en base humeda" = preexp$`Rendimiento BH (g)`,
                          "Rendimiento obtenido en materia seca" = preexp$`Rendimiento MS (g)`) 
 View(datos_seca)
 
 
-matplot(preexp$`día de cosecha`, datos_seca, , type = "l", 
-        main = "Estimaci?n del rendimiento de Azolla en Materia Seca",
+matplot(preexp$`Día de cosecha`, datos_seca, , type = "l", 
+        main = "Estimación del rendimiento de Azolla en Materia Seca",
         xlab = "Cosecha (días)", ylab = "Cantidad (g)",
         col = c("blue","red"),
         lty = 2, lwd = 2, xaxt = "n", yaxt = "n", ylim = c(0, 5000))
 
-  grid(nx = NA, ny = NULL, lwd = 1, lty = 1, col = ("gray"))
+grid(nx = NA, ny = NULL, lwd = 1, lty = 1, col = ("gray"))
 
 axis(1, at = c(8, 16, 24, 32, 40))
 axis(2, at = c(50, 850, 1650, 2450, 3250, 4050, 4850), las = 1)
@@ -73,15 +73,16 @@ legend(x = "top",
        bty = "n")                
 
 
-# Comparacion de la dosificacion con esti?rcol de cuy
+# Comparación de la dosificación con estiércol de cuy
 
 library(readxl)
 exp <- read_excel("~/MEGA/Daniel Mancilla. Fertilización de Azolla con Curinaza/exp.xlsx", 
-                     range = "A1:G6")
+                     range = "A1:B10")
 View(exp)
 
 
-# Librer?as para el an?lisis
+# Librerías para el análisis
+
 library(datasets)
 library(ggplot2)
 library(multcompView)
@@ -89,9 +90,11 @@ library(dplyr)
 
 
 # cargando y comprobando los datos
+
 str(exp)
 
-# ANOVA
+# ANOVA de un solo factor
+
 anova <- aov(Rendimiento ~ Tratamiento, data = exp)
 summary(anova)
 
@@ -99,29 +102,32 @@ exp$Tratamiento <- as.factor(exp$Tratamiento)
 
 
 # Tukey's test
+
 tukey <- TukeyHSD(anova)
 print(tukey)
 
 # compact letter display
+
 cld <- multcompLetters4(anova, tukey)
 print(cld)
 
 # table with factors and 3rd quantile
+
 Tk <- group_by(exp, Tratamiento) %>%
   summarise(mean=mean(Rendimiento), quant = quantile(Rendimiento, probs = 0.75)) %>%
   arrange(desc(mean))
 
 # extracting the compact letter display and adding to the Tk table
+
 cld <- as.data.frame.list(cld$Tratamiento)
 Tk$cld <- cld$Letters
 
 print(Tk)
 
-# Gr?fico de cajas y bigotes
-
+# Gráfico de cajas y bigotes
 
 box_plot <- boxplot(exp$Rendimiento~exp$Tratamiento, main = "Rendimiento de Azolla por tratamiento",
-                    xlab = "Esti?rcol de cuy", ylab = "Rendimiento (g)",
+                    xlab = "estiércol de cuy", ylab = "Rendimiento (g)",
                     col = c("green","blue","red"), ylim=c(min(exp$Rendimiento) , 1.1*max(exp$Rendimiento)),
                     horizontal = F, names = c("T0", "T1", "T2"), las = 1)
 
@@ -143,8 +149,10 @@ legend(x = "top",
        bty = "n",
        pch = 7)  
 
-over <- 0.1*max( box_plot$stats[nrow(box_plot$stats),] )
-text( c(1:nlevels(exp$Tratamiento)) , box_plot$stats[nrow(box_plot$stats),]+over , LABELS[,1])
+over <- 0.1*max(box_plot$stats[nrow(box_plot$stats),])
+
+text(c(1:nlevels(exp$Tratamiento)), box_plot$stats[nrow(box_plot$stats),
+                                                     ]+over, LABELS[,1])
 
 # Media e IC al 95% para la media por tratamiento (g)
 
